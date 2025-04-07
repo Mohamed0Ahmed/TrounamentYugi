c8f47a3840d353070d98495bab7585ca3ae85710
Mohamed Ahmed <mahmmaad96@gmail.com>
2025-04-07 14:16:00 +0200

    Add Images Static

--- src/app/player/forbidden-cards/forbidden-cards.component.ts
+++ src/app/player/forbidden-cards/forbidden-cards.component.ts
@@ -9,32 +9,36 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 export class ForbiddenCardsComponent implements OnInit {
   spellCards: string[] = [
     './assets/images/Spell Cards/1.jpg',
-    './assets/images/Spell Cards/2.jpg',
-    './assets/images/Spell Cards/3.jpg',
     './assets/images/Spell Cards/4.jpg',
     './assets/images/Spell Cards/5.jpg',
     './assets/images/Spell Cards/6.jpg',
-    './assets/images/Spell Cards/7.jpg',
-    './assets/images/Spell Cards/8.jpg',
-    './assets/images/Spell Cards/9.jpg',
-    './assets/images/Spell Cards/10.jpg',
-    './assets/images/Spell Cards/11.jpg',
     './assets/images/Spell Cards/12.jpg',
-    './assets/images/Spell Cards/13.jpg',
     './assets/images/Spell Cards/14.jpg',
     './assets/images/Spell Cards/15.jpg',
     './assets/images/Spell Cards/16.jpg',
-    './assets/images/Spell Cards/17.jpg',
     './assets/images/Spell Cards/18.jpg',
     './assets/images/Spell Cards/19.jpg',
     './assets/images/Spell Cards/20.jpg',
     './assets/images/Spell Cards/21.jpg',
+    './assets/images/Spell Cards/24.jpg',
+    './assets/images/Spell Cards/25.jpg',
+    './assets/images/Spell Cards/26.jpg',
+    './assets/images/Spell Cards/13.jpg',
+    './assets/images/Spell Cards/2.jpg',
+    './assets/images/Spell Cards/3.jpg',
+    './assets/images/Spell Cards/7.jpg',
+    './assets/images/Spell Cards/17.jpg',
+    './assets/images/Spell Cards/8.jpg',
+    './assets/images/Spell Cards/9.jpg',
+    './assets/images/Spell Cards/10.jpg',
+    './assets/images/Spell Cards/11.jpg',
     './assets/images/Spell Cards/22.jpg',
     './assets/images/Spell Cards/23.jpg',
   ];
   trapCards: string[] = [
-    './assets/images/Trap Cards/1.jpg',
+    './assets/images/Trap Cards/11.jpg',
     './assets/images/Trap Cards/2.jpg',
+    './assets/images/Trap Cards/1.jpg',
     './assets/images/Trap Cards/3.jpg',
     './assets/images/Trap Cards/4.jpg',
     './assets/images/Trap Cards/5.jpg',
@@ -43,14 +47,14 @@ export class ForbiddenCardsComponent implements OnInit {
     './assets/images/Trap Cards/8.jpg',
     './assets/images/Trap Cards/9.jpg',
     './assets/images/Trap Cards/10.jpg',
-    './assets/images/Trap Cards/11.jpg',
     './assets/images/Trap Cards/12.jpg',
     './assets/images/Trap Cards/13.jpg',
   ];
   monsters: string[] = [
-    './assets/images/Monster/1.jpg',
-    './assets/images/Monster/2.jpg',
+    './assets/images/Monster/13.jpg',
     './assets/images/Monster/3.jpg',
+    './assets/images/Monster/2.jpg',
+    './assets/images/Monster/1.jpg',
     './assets/images/Monster/4.jpg',
     './assets/images/Monster/5.jpg',
     './assets/images/Monster/6.jpg',
@@ -64,6 +68,7 @@ export class ForbiddenCardsComponent implements OnInit {
 
   selectedImage: string | null = null;
   isModalOpen = false;
+  isModalOpenAll = false;
   ngOnInit(): void {}
 
   customOptions: OwlOptions = {
@@ -95,9 +100,13 @@ export class ForbiddenCardsComponent implements OnInit {
     this.selectedImage = image;
     this.isModalOpen = true;
   }
+  openAll(): void {
+    this.isModalOpenAll = true;
+  }
 
   closeModal(): void {
     this.isModalOpen = false;
+    this.isModalOpenAll = false;
     this.selectedImage = null;
   }
 }
